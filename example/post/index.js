e242bc96074ffe19bda0aefb60b5886c148ed9f6
Luis Montes <monteslu@gmail.com>
2012-11-09 16:30:10 -0700

    form post demo

--- example/post/index.js
+++ example/post/index.js
@@ -18,29 +18,20 @@ onload = function() {
 
   var http = require('http');
   http.createServer(function (req, res) {
-    
-
-      if (req.method == 'POST') {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
-
-            //var POST = qs.parse(body);
-            // use POST
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(body + '\n');
-
-        });
-  }else{
-
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  }
+    if (req.method == 'POST') {
+      var body = '';
+      req.on('data', function (data) {
+          body += data;
+      });
+      req.on('end', function () {
+        res.end(body + '\n');
+      });
+    }else{
+      res.end('Hello World\n');
+    }
 
-    
-  }).listen(1337, '127.0.0.1');
-  console.log('Server running at http://127.0.0.1:1337/');
+  }).listen(1338, '127.0.0.1');
+  console.log('Server running at http://127.0.0.1:1338/');
 
 };
